574b67aa46bb3a24829b2c7015dfb03728b7c9e8
steve <smt9814@rit.edu>
2018-01-27 23:47:36 -0500

    added art

--- bbpython/serverInterface.py
+++ bbpython/serverInterface.py
@@ -1,9 +1,10 @@
-
 import os
+import pycurl
+from StringIO import StringIO
 
 
 def pingServer():
-	hostname = "google.com" #example
+	hostname = "google.com" #TODO: Change this to the real Host
 	response = os.system("ping -c 1 " + hostname + "")
 	if 0 == response:
 		return True
@@ -15,4 +16,15 @@ def setData(charlist):
 	pass
 
 def getData(charlist):
+	buffer = StringIO()
+	c = pycurl.Curl()
+	c.setopt(c.URL, 'http://pycurl.io/')
+	c.setopt(c.WRITEDATA, buffer)
+	c.perform()
+	c.close()
+
+	body = buffer.getvalue()
+	# Body is a string in some encoding.
+	# In Python 2, we can print it without knowing what the encoding is.
+	print(body)
 	pass
