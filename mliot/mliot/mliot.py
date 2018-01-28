3c59cdd0162b60921c5e23e1ae1531262c0d351c
ssnover95 <sws7379@rit.edu>
2018-01-27 21:42:18 -0500

    Successful parsing of sqlite row objects

--- mliot/mliot/mliot.py
+++ mliot/mliot/mliot.py
@@ -71,31 +71,60 @@ def news_feed():
 def user_profile():
     backend_database = getDatabase(app.config["DATABASE"])
     cursor = backend_database.execute('select hw_id, hunger, happiness, energy from characters order by hw_id desc')
-    character_list = cursor.fetchall()
-    character_dict = {}
-    if character_list != []:
-        character_dict = {'name': character_list[0], 'values': [character_list[1], character_list[2], character_list[3]]}
-    return render_template('profile.html', chars=character_dict)
+    character_data = cursor.fetchall()
+    # print(tuple(character_list[0]))
+    character_list = []
+    for datum in character_data:
+        character_list += [{'id':tuple(datum)[0], 'hunger':tuple(datum)[1], 'happiness':tuple(datum)[2], 'energy':tuple(datum)[3]}]
+    return render_template('profile.html', chars=character_list)
 
 
-@app.route('/new_char', methods=['POST'])
+@app.route('/new_char', methods=['GET', 'POST'])
 def new_character():
+    # first check if the char is already in our character table
     db = getDatabase(app.config["DATABASE"])
-    db.execute('insert into island_of_misfit_mupets (hw_id, hunger, happiness, energy) values (?, ?, ?, ?)',
-                request.form['id'], request.form['hunger'], request.form['happiness'], request.form['energy'])
+
+    # get the values from the request
+    my_id = int(request.args.get('id'))
+    my_hunger = int(request.args.get('hunger'))
+    my_happiness = int(request.args.get('happiness'))
+    my_energy = int(request.args.get('energy'))
+
+    cursor = db.execute('select hw_id from characters')
+    id_list = cursor.fetchall()
+    if my_id in id_list:
+        # don't add it, update instead
+        print("Old id, updating")
+        db.execute("""update characters
+                      set hunger = (?),
+                          happiness = (?),
+                          energy = (?)
+                      where hw_id = (?);""",
+                   my_hunger, my_happiness, my_energy, my_id)
+    else:
+        print("New id, adding it")
+        db.execute('insert into characters (hw_id, hunger, happiness, energy) values (?, ?, ?, ?)',
+                   [my_id, my_hunger, my_happiness, my_energy])
     db.commit()
     flash("New character added.")
-    return "Something"
+    return redirect(url_for('user_profile'))
+
+
+@app.route('/test_query', methods=['GET', 'POST'])
+def test_query_capture():
+    test = request.args.get('test')
+    print(test)
+    return redirect(url_for('news_feed'))
 
 
 @app.route('/addCharacter', methods=['POST'])
 def add_character():
-    if not session.get('logged_in'):
-        abort(401)
+    # if not session.get('logged_in'):
+    #    abort(401)
     db = getDatabase(app.config["DATABASE"])
     # also grab the unassigned characters in connected nodes
-    # db.execute('insert into characters (hw_id, hunger, happiness, energy) values (?, ?)',
-    #           [request.form[]])
+    db.execute('insert into characters (hw_id, hunger, happiness, energy) values (?, ?, ?, ?)',
+              [request.form['id'], request.form['hunger'], request.form['happiness'], request.form['energy']])
     db.commit()
     flash('Added a new character!')
 
